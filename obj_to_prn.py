8b963aea40f3f4c39eb99466d63b945dd1af8b26
JomoPipi <ronaldmcorona@gmail.com>
2024-05-03 21:17:46 -0700

    stuff changes

--- obj_to_prn.py
+++ obj_to_prn.py
@@ -5,10 +5,9 @@ from math import *
 
 import sys, os
 
-obj_file = sys.argv[1]
-prn_file = sys.argv[2]
 
-spindle_speed = 9001
+
+spindle_speed = 12000
 cutter_vel = 30 # 50 being max, for some reason
 clearance_height = 10
 
@@ -17,33 +16,68 @@ clearance_height = 10
 # this happens to be the case when converting a long line to a curve prior to export in blender
 # so big stretches of "l" lines will be treated as a delimiter between paths, and nothing else
 
-with open(obj_file) as src, open(prn_file, 'w') as dest:
+def obj_to_prn(obj_data, prn_file):
+    with open(prn_file, 'w') as dest:
+        pv = None
+        prn = ";;^IN;!MC0;V50.0;^PR;Z0,0,20000;^PA;!RC{};!MC1;".format(spindle_speed)
+        seen_line = True
 
-    pv = None
-    prn = ";;^IN;!MC0;V50.0;^PR;Z0,0,20000;^PA;!RC{};!MC1;".format(spindle_speed)
-    seen_line = True
+        for line in obj_data.split('\n'):
 
-    for line in src:
+            if line.startswith('v'):
+                xyz = line.strip().split()[1:]
+                x = float(xyz[0])
+                y = float(xyz[1])
+                z = float(xyz[2])
+                px = int(x*100)
+                py = int(y*100)
+                pz = int(z*100)
+                if seen_line:
+                    prn += "V{:.1f};Z{},{},1000;\n".format(cutter_vel, px, py)
+                    seen_line = False
+                zline = "Z{},{},{};\n".format(px, py, pz)
+                prn += zline
+                pv = (px, py, pz)
 
-        if line.startswith('v'):
-            xyz = line.strip().split()[1:]
-            x = float(xyz[0])
-            y = float(xyz[1])
-            z = float(xyz[2])
-            px = int(x*100)
-            py = int(y*100)
-            pz = int(z*100)
-            if seen_line:
-                prn += "V{:.1f};Z{},{},1000;\n".format(cutter_vel, px, py)
-                seen_line = False
-            zline = "Z{},{},{};\n".format(px, py, pz)
-            prn += zline
-            pv = (px, py, pz)
+            if not seen_line and line.startswith('l'):
+                prn += "V50.0;Z{},{},1000;\n".format(pv[0], pv[1])
+                seen_line = True
 
-        if not seen_line and line.startswith('l'):
-            prn += "V50.0;Z{},{},1000;\n".format(pv[0], pv[1])
-            seen_line = True
+        prn += "V50.0;^PA;!ZM0;!MC0;^IN;"
+        dest.write(prn)
 
-    prn += "V50.0;^PA;!ZM0;!MC0;^IN;"
-    dest.write(prn)
-                
+def fobj_to_prn(obj_file, prn_file):
+    with open(obj_file) as src, open(prn_file, 'w') as dest:
+
+        pv = None
+        prn = ";;^IN;!MC0;V50.0;^PR;Z0,0,20000;^PA;!RC{};!MC1;".format(spindle_speed)
+        seen_line = True
+
+        for line in src:
+
+            if line.startswith('v'):
+                xyz = line.strip().split()[1:]
+                x = float(xyz[0])
+                y = float(xyz[1])
+                z = float(xyz[2])
+                px = int(x*100)
+                py = int(y*100)
+                pz = int(z*100)
+                if seen_line:
+                    prn += "V{:.1f};Z{},{},1000;\n".format(cutter_vel, px, py)
+                    seen_line = False
+                zline = "Z{},{},{};\n".format(px, py, pz)
+                prn += zline
+                pv = (px, py, pz)
+
+            if not seen_line and line.startswith('l'):
+                prn += "V50.0;Z{},{},1000;\n".format(pv[0], pv[1])
+                seen_line = True
+
+        prn += "V50.0;^PA;!ZM0;!MC0;^IN;"
+        dest.write(prn)
+
+if __name__ == "__main__":
+    obj_file = sys.argv[1]
+    prn_file = sys.argv[2]
+    fobj_to_prn(obj_file, prn_file)
